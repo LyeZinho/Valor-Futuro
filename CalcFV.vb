f27f0e36a4aa53617b9e3fd672fcb95ae37454ef
Pedro Kaleb lol <pkgamespk1223pk@gmail.com>
2021-10-14 16:18:13 +0100

    ✨ Atualização 0.2

---
+++ CalcFV.vb
@@ -0,0 +1,61 @@
+﻿Public Module CalculatorVF
+    ''' <summary>
+    ''' Criação biblioteca para calculo valor futuro
+    ''' explicaões de uso
+    ''' </summary>
+
+
+    'SetPeriod(_iput As integer)        [Input]
+    'SetTaxMonth(_input As Integer)     [Input]
+    'SetTaxYearly(_input As Integer)    [Input]
+    'SetStartVaule(_input As Double)    [Input]
+    'SetYearlyDeposit(_input As Double) [Input] 
+    'SetMonthDeposit(_input As Double)  [Input]
+    'YearlyFV() As Double               [Function]
+    'MonthlyFV() As Double              [Function]
+
+    'Variaveis de operação
+    Dim Periodo As Integer
+    Dim TaxaAnual, TaxaMensal As Integer
+    Dim DepositoMensal, DepositoAnual As Double
+    Dim ValorInicial As Double
+
+    'Alimentar valores
+    Sub SetPeriod(_input As Integer)
+        Periodo = _input
+    End Sub
+
+    Sub SetTaxMonth(_input As Integer)
+        TaxaMensal = _input
+    End Sub
+
+    Sub SetTaxYearly(_input As Integer)
+        TaxaAnual = _input
+    End Sub
+
+    Sub SetStartVaule(_input As Double)
+        ValorInicial = _input
+    End Sub
+
+    Sub SetYearlyDeposit(_input As Double)
+        DepositoAnual = _input
+    End Sub
+
+    Sub SetMonthDeposit(_input As Double)
+        DepositoMensal = _input
+    End Sub
+
+    'Operações e calculos
+    Function YearlyFV() As Double
+        Dim Result As Double
+        Result = DepositoAnual * (((1 + TaxaAnual) ^ Periodo - 1) / TaxaAnual) + ValorInicial * (1 + TaxaAnual) ^ Periodo
+        Return Result
+    End Function
+
+    Function MonthlyFV() As Double
+        Dim Result As Double
+        Result = DepositoMensal * (((1 + TaxaMensal) ^ Periodo - 1) / TaxaMensal) + ValorInicial * (1 + TaxaMensal) ^ Periodo
+        Return Result
+    End Function
+
+End Module
